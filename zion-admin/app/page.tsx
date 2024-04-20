8c9e04d75a7fa994aff3040121ae38d6fc5aba8d
mukisaivan <106663129+mukisaivan@users.noreply.github.com>
2024-04-20 23:26:13 +0300

    for the matter of commenting

--- zion-admin/app/page.tsx
+++ zion-admin/app/page.tsx
@@ -1,13 +1,8 @@
 "use client" 
 import { useRouter } from "next/navigation";
-
-
 export default function Home() {
   const router = useRouter()
   const navv = router.push('/home')
-
-
-
   return (
     <>
       {navv}
