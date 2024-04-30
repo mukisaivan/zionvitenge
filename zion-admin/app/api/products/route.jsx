056a96d172a84badc0e9fad1ef372c18d4279ffe
mukisaivan <106663129+mukisaivan@users.noreply.github.com>
2024-04-30 21:29:25 +0300

    finished the get function

--- zion-admin/app/api/products/route.jsx
+++ zion-admin/app/api/products/route.jsx
@@ -1,5 +1,16 @@
-export default function handler(req, res) {
-  if (req == 'POST') {
-    //do something
+import {Product} from "@/models/Product";
+
+export default async function handler(req, res) {
+
+  const {method} = req 
+
+
+  if (method== 'GET') {
+    
+    if (req.query?.id) {
+      res.json(await Product.findOne({_id: req.query.id}))
+    } else {
+      res.json(await Product.find());
+    }
   }
 }
