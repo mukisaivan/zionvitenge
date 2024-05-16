4448a71fed5d376dc14c94a11764b46e9a219d21
mukisaivan <106663129+mukisaivan@users.noreply.github.com>
2024-05-16 23:38:34 +0300

    edit product page

--- zion-admin/app/home/products/components/EditPdt.jsx
+++ zion-admin/app/home/products/components/EditPdt.jsx
@@ -10,33 +10,10 @@ function EditPdt({ id }) {
     router.push(`/home/edit/${id}`)
   }
 
-  async function handleedit() {
-    try {
-      const res = fetch(`http://localhost:3000/api/products/${id}`, {
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({}),
-      });
-
-      if (!res.ok) {
-        throw new Error("Failed to fetch topic");
-      }
-      router.refresh();
-      router.push("/home/products");
-      router.refresh();
-
-    } catch (error) {
-      console.log("+++++++++++++++++++++++++ error", error);
-    }
-
-    
-  }
-
   const editbtn = (
     <button
       type="button"
-      onClick={handleedit}
+      onClick={movetoeditpage}
       className=" bg-blue-700  px-5 p-1 text-white rounded-lg"
     >
       Edit
--- zion-admin/app/home/products/edit/page.jsx
+++ zion-admin/app/home/products/edit/page.jsx
@@ -2,8 +2,38 @@ import ProductForm from '@/app/components/ProductForm'
 import React from 'react'
 
 function EditPage() {
+
+    
+  async function handleedit() {
+    try {
+      const res = fetch(`http://localhost:3000/api/products/${id}`, {
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({}),
+      });
+
+      if (!res.ok) {
+        throw new Error("Failed to fetch topic");
+      }
+      router.refresh();
+      router.push("/home/products");
+      router.refresh();
+
+    } catch (error) {
+      console.log("+++++++++++++++++++++++++ error", error);
+    }
+
+    
+  }
+
+
+
   return (
-    <ProductForm/>
+    <>
+      <h1>Edit Product Page</h1>
+    </>
+    // <ProductForm/>
   )
 }
 
