464d35a1382264d2c42c8c59c62bbf695e7c567d
mukisaivan <106663129+mukisaivan@users.noreply.github.com>
2024-06-02 15:50:13 +0300

    image uplaod to firebae

--- zion-admin/app/firebase.jsx
+++ zion-admin/app/firebase.jsx
@@ -1,17 +1,32 @@
-// firebaseConfig.js
-import { initializeApp } from 'firebase/app';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
 import { getStorage } from 'firebase/storage';
 
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
+  apiKey: "AIzaSyDFGos6NSXyGcD5s7vKNQ-NcRCNqr8KEY0",
+  authDomain: "zion-vitenge.firebaseapp.com",
+  projectId: "zion-vitenge",
+  storageBucket: "zion-vitenge.appspot.com",
+  messagingSenderId: "746083264325",
+  appId: "1:746083264325:web:274764aefb54a71ece802c",
+  measurementId: "G-6XFPZ8XNJL"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 const storage = getStorage(app);
 
 export { storage };
+
+
+
+
+
