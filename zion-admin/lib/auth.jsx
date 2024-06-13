4410ea8decd5268b9a428a5ab6e8c4832b2620d8
mukisaivan <106663129+mukisaivan@users.noreply.github.com>
2024-06-13 22:06:23 +0300

    admin auth and clarifying extensions

--- zion-admin/lib/auth.jsx
+++ zion-admin/lib/auth.jsx
@@ -12,7 +12,15 @@ export const authConfig = {
             clientSecret: process.env.GOOGLE_SECRET,
         }),
     ],
-    adapter: MongoDBAdapter(clientPromise)
+    adapter: MongoDBAdapter(clientPromise),callbacks: {
+    session: ({session,token,user}) => {
+      if (adminEmails.includes(session?.user?.email)) {
+        return session;
+      } else {
+        return false;
+      }
+    },
+  },
 };
 
 export async function loginIsRequiredServer() {
