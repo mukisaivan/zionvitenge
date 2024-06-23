1af0b8c301fed1da851321ef8b64f3dbe8678d23
mukisaivan <106663129+mukisaivan@users.noreply.github.com>
2024-06-23 23:58:25 +0300

    minor changes

--- zion-front/app/components/WhatsAppButton.jsx
+++ zion-front/app/components/WhatsAppButton.jsx
@@ -6,14 +6,14 @@ import { FaWhatsapp } from "react-icons/fa6";
 const WhatsAppButton = ({ phoneNumber }) => {
   const firstwhatsapp = (
     <a
-    href={`https://wa.me/${phoneNumber}?text=hello`}
+    href={`https://wa.me/${phoneNumber}?text=hey theres this product i want do you deliver?`}
     className="fixed bottom-10 right-10 bg-green-500 text-white p- rounded-full shadow-lg"
       target="_blank"
       rel="noopener noreferrer"
     >
       <div className=" p-5 flex gap-2">
         <FaWhatsapp color="white" className="w-10 h-10 md:w-10 md:h-5" />
-        Chat with us on WhatsApp
+        Chat on WhatsApp
       </div>
     </a>
   );
